91d24a998f6e66ba135e551a96ebc5e201903844
michael <michaelgdimmitt@gmail.com>
2024-01-15 13:20:29 -0500

    bash script looks at a file and tells what deps may be used in the program

---
+++ get-deps.bash
@@ -0,0 +1,21 @@
+
+echo ;
+echo potential dependencies: ;
+echo -n "  ";
+
+filename='../traverse-get-mp3-metadata/get-mp3-data.bash'
+
+compgenItems=$(compgen -b  | tr "\n" "|" | rev | cut -c2- | rev | tr "\|" "\n" | grep -x '[[:alpha:]]*' | tr "\n" "|" | rev | cut -c2- | rev)
+
+cat $filename  | 
+  grep -vE '#|echo \"' |
+  tr " " "\n" | tr -s '\n' | tr "\(" "\n" |
+  grep -x '[[:alpha:]]*' | 
+  grep -v -E "$compgenItems"  |
+  grep -vE "if|then|else|fi|col|row|as|done|while|rm|cat|add|length|quiet|do|map|cp|v" |
+  uniq |
+  tr "\n" " ";
+echo ;
+
+# combine the above with `find . -name '*sh'` and `read` to evaluate the whole codebase.
+# may need to combine with a common variables file delimited by newlines or '|' characters.
